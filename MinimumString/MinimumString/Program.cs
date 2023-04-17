43775e3e5324fadbe4cb7481d19607d3ced3e31c
MSRbuddy <meghanadasari222@gmail.com>
2023-04-17 22:59:14 +0530

    Added : Find Minimum String value!

---
+++ MinimumString/MinimumString/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MinimumString
+{
+    internal class Program
+    {
+        static void Minimum(string x, string y, string z)
+        {
+            string min = x;
+            if (x.Length <= y.Length && x.Length <= z.Length)
+                min = x;
+            else if (y.Length <= x.Length && y.Length <= z.Length)
+                min = y;
+            else
+                min = z;
+            Console.WriteLine("Given values are : " + x + " " +
+                              y + " " + z);
+            Console.WriteLine("Minimum value is : " + min + "\n");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Find Minimum String!*****\n");
+            Minimum("PINEAPPLE", "ORANGE", "MANGO");
+        }
+    }
+}
