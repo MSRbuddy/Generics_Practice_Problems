31c36eb5fa2f83cac05ecbb343164992b9ffdd3e
MSRbuddy <meghanadasari222@gmail.com>
2023-04-17 23:02:53 +0530

    Added : Find Minimum value using Generic-Class extends Comparable!

---
+++ MinGenericClass/MinGenericClass/GenereicClass.cs
@@ -0,0 +1,35 @@
+﻿namespace MinGenericClass
+{
+    public class GenericMinimum<T> where T : IComparable<T>
+    {
+        private T firstValue;
+        private T secondValue;
+        private T thirdValue;
+        public GenericMinimum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T Minimum(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) < 0 && firstValue.CompareTo(thirdValue) < 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) < 0 && secondValue.CompareTo(thirdValue) < 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) < 0 && thirdValue.CompareTo(secondValue) < 0)
+            {
+                return thirdValue;
+            }
+            else
+            {
+                throw new Exception("Given values are same");
+            }
+
+        }
+    }
+}
---
+++ MinGenericClass/MinGenericClass/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MinGenericClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Find Minimum value using Generic-Method*****");
+            Console.WriteLine("----------------------------------------------------");
+
+            int MinInt = GenericMinimum<int>.Minimum(57, 83, 19);
+            Console.WriteLine($"\nMinimum Int value is: {MinInt}");
+
+            float MinFloat = GenericMinimum<float>.Minimum(46.9f, 11.7f, 21.3f);
+            Console.WriteLine($"\nMinimum Float value is: {MinFloat}");
+
+            string MinString = GenericMinimum<string>.Minimum("Keerthi", "Bhargavi", "Bharath");
+            Console.WriteLine($"\nMinimum String value is: {MinString}");
+        }
+    }
+}
