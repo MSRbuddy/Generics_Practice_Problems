8d3b4c6df7d6e46dd0eef0d8d5cc63eac507a529
MSRbuddy <meghanadasari222@gmail.com>
2023-04-17 22:50:20 +0530

    Added : Delete corresponding element from an Array!

---
+++ DeleteElement/DeleteElement/DeleteArray.cs
@@ -0,0 +1,81 @@
+﻿namespace DeleteElement
+{
+    
+    public class DeleteArray
+    { 
+        public static void DeleteInt(int[] n)
+        {
+            int i = 0, x;
+
+            Console.WriteLine("\nElements before deletion:");
+            for (i = 0; i < 7; i++)
+            {
+                Console.Write(intArr[i] + " ");
+            }
+
+            // Let's say the position to delete the item is 6 i.e. intArr[5]
+            x = 6;
+            // Shifting elements
+            for (i = x - 1; i < 6; i++)
+            {
+                intArr[i] = intArr[i + 1];
+            }
+            Console.WriteLine("\nElements after deletion: ");
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write(intArr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void DeleteDouble(double[] n)
+        {
+            int i = 0, x;
+
+            Console.WriteLine("\nElements before deletion:");
+            for (i = 0; i < 7; i++)
+            {
+                Console.Write(doubleArr[i] + " ");
+            }
+
+            // Let's say the position to delete the item is 3 i.e. doubleArr[2]
+            x = 3;
+            // Shifting elements
+            for (i = x - 1; i < 6; i++)
+            {
+                doubleArr[i] = doubleArr[i + 1];
+            }
+            Console.WriteLine("\nElements after deletion: ");
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write(doubleArr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void DeleteChar(char[] n)
+        {
+            int i = 0, x;
+
+            Console.WriteLine("\nElements before deletion:");
+            for (i = 0; i < 7; i++)
+            {
+                Console.Write(charArr[i] + " ");
+            }
+
+            // Let's say the position to delete the item is 5 i.e. charArr[4]
+            x = 5;
+            // Shifting elements
+            for (i = x - 1; i < 6; i++)
+            {
+                charArr[i] = charArr[i + 1];
+            }
+            Console.WriteLine("\nElements after deletion: ");
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write(charArr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DeleteElement/DeleteElement/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DeleteElement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Delete an Elememt from the array*****!");
+            Console.WriteLine("--------------------------------------------------");
+            
+            int[] intArr = { 10, 20, 30, 40, 50, 60, 70 };
+            double[] doubleArr = { 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5 };
+            char[] charArr = { 'M', 'E', 'G', 'H', 'A', 'N', 'A' };
+
+            DeleteArray.DeleteInt(intArr);
+            DeleteArray.DeleteDouble(doubleArr);
+            DeleteArray.DeleteChar(charArr);
+        }
+    }
+}
