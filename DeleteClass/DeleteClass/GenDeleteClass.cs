93d327080ae3e0d3e749d5d2a7a1c4b4e763de68
MSRbuddy <meghanadasari222@gmail.com>
2023-04-17 22:54:05 +0530

    Added : Delete element from an Array using Generic-Class!

---
+++ DeleteClass/DeleteClass/GenDeleteClass.cs
@@ -0,0 +1,29 @@
+﻿namespace DeleteClass
+{
+    internal class GenDeleteClass<T>
+    {
+        public static void DeleteMethod(T[] n)
+        {
+            int i = 0, x;
+            Console.WriteLine("\nElements before deletion:");
+            for (i = 0; i < 7; i++)
+            {
+                Console.Write(n[i] + " ");
+            }
+
+            // Let's say the position to delete the item is 4 i.e. intArr[5]
+            x = 1;
+            // Shifting elements
+            for (i = x - 1; i < 6; i++)
+            {
+                n[i] = n[i + 1];
+            }
+            Console.WriteLine("\nElements after deletion: ");
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write(n[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DeleteClass/DeleteClass/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DeleteClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Delete an Elememt from the array using Generic-Class*****!");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            int[] intArr = { 11, 23, 35, 47, 59, 61, 75, };
+            double[] doubleArr = { 9.5, 8.4, 7.3, 6.2, 7.1, 4.6, 3.8, };
+            char[] charArr = { 'A', 'E', 'I', 'O', 'U', 'B', 'M', };
+
+            GenDeleteClass<int>.DeleteMethod(intArr);
+            GenDeleteClass<double>.DeleteMethod(doubleArr);
+            GenDeleteClass<char>.DeleteMethod(charArr);
+        }
+    }
+}
